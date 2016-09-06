e013652f89f808874045f4af4c408584a09a4ed9
Jared Thomas <jjthom87@yahoo.com>
2016-09-06 17:27:24 -0400

    maybe

--- models/index.js
+++ models/index.js
@@ -12,11 +12,7 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   console.log("using defaults");
-  var sequelize = new Sequelize('companyresearch4_db', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  })
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
