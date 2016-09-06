028de2acf11c1d9ad60c0ca15721cab71fa42f29
Jared Thomas <jjthom87@yahoo.com>
2016-09-06 14:53:18 -0400

    maybe

--- models/application.js
+++ models/application.js
@@ -13,7 +13,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Application.belongsTo(models.User);
+        // Application.belongsTo(models.User);
       }
     }
   });
